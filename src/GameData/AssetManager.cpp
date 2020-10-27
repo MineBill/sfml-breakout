b72a895e694d63e31747c987a17e4d9f9af34a92
MineBill <terrestrialstudio@gmail.com>
2020-10-27 12:53:55 +0200

    fixed: Bug when generating texture

--- src/GameData/AssetManager.cpp
+++ src/GameData/AssetManager.cpp
@@ -4,7 +4,9 @@
 void AssetManager::LoadAsset(const std::string &name, const std::string &path) {
     auto texture = std::make_shared<sf::Texture>();
     if (!texture->loadFromFile(path)) {
-        texture->loadFromMemory(GenerateCheckerboard(), 64 * 64 * 4);
+        auto pixles = GenerateCheckerboard();
+        texture->create(64, 64);
+        texture->update(pixles);
     }
     m_assets[name] = texture;
 }
@@ -17,9 +19,9 @@ sf::Uint8 *AssetManager::GenerateCheckerboard(int width, int height) {
     sf::Uint8 *pixels =
         new sf::Uint8[width * height * 4]; // 4: number of pixel components
 
-    for (int x = 0; x < width; ++x) {
-        for (int y = 0; y < height; ++y) {
-            auto i = x * height + y * width;
+    for (int x = 0; x < width * 4; x += 4) {
+        for (int y = 0; y < height * 4; y += 4) {
+            auto i = x * height + y;
             pixels[i + 0] = 255;
             pixels[i + 1] = 1;
             pixels[i + 2] = 1;
