e7a8fb0b1b3563d9430feb3a725e1e984483a087
MineBill <terrestrialstudio@gmail.com>
2020-10-27 12:56:02 +0200

    changes: Small comment addition to make intent clearer

--- src/States/SplashState.cpp
+++ src/States/SplashState.cpp
@@ -7,6 +7,10 @@ SplashState::SplashState(GameDataRef t_data) : m_data(t_data) {
 
 void SplashState::OnInit() {
     fmt::print("SplashState::OnInit\n");
+
+    // LoadAsset should fail to find the texture named `awdnauwnd`
+    // and instead should generate a default texture to indicate
+    // a texture that is not found
     m_data->assets.LoadAsset("foo", "awdnauwnd");
 
     auto texture = m_data->assets.GetAsset("foo").get();
